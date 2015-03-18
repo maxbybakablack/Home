49b60106f19be1a4e2e3e863b84ded90efd8ff76
Schlipak <g.de.matos@free.fr>
2015-03-18 21:49:00 +0100

    Moved js app file to new dir

--- app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	"use strict";
-
-	$(window).scroll(function() {
-		var offset = $(this).scrollTop();
-		console.log("ijpijp");
-	});
-});
---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	"use strict";
+
+	$('#pagepiling').pagepiling({
+		menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: [],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: true,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['section1', 'section2', 'section3']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+	});
+});
